17f51180fb19cdb70df3fe8b8b78edab7c3de7fc
Alon Feller <alon.feller16@gmail.com>
2022-04-27 15:09:15 +0300

    15:09

--- apps/keep/cmps/note-preview.jsx
+++ apps/keep/cmps/note-preview.jsx
@@ -1,13 +1,11 @@
 const { Link } = ReactRouterDOM
 
 
-export function NotePreview({ car: note }) {
-    return <Link to={`/car/${note.id}`}>
+export function NotePreview({ note: note }) {
+    return <Link to={`/keep/note/${note.id}`}>
         <article className="Note-preview" >
-            <h3>type : {note.vendor}</h3>
-            <h3>Speed : {note.speed}</h3>
+            <h3>type : {note.type}</h3>
             <div className="img-container">
-                <img src={`assets/img/${note.vendor}.jpg`} />
             </div>
         </article>
     </Link>
