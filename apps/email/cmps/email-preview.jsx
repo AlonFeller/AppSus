8f564d58553f0b61e829e061dab369a535105946
Omer Levi <omerlevi223@gmail.com>
2022-04-28 19:17:53 +0300

    dfogm

--- apps/email/cmps/email-preview.jsx
+++ apps/email/cmps/email-preview.jsx
@@ -5,9 +5,16 @@ export function EmailPreview({ email, onReadingEmail }) {
     console.log(email.message)
     return <article className="email-preview" >
         <div className="email-preview-msg" onClick={() => onReadingEmail(email.id)}>
+            <div class="btns">
+                <button class="trash-btn">delete</button>
+                <button class="starred-btn">star</button>
+                <button class="read-btn">read/unread</button>
+            </div>
             <div>To: {email.to}</div>
-            <div className="mail-subject"><LongTxt txt={email.subject} chars={25} /></div>
-            <div className="mail-message"><LongTxt txt={email.body} chars={70} /></div>
+            <div class="message-container">
+                <div className="mail-subject"><LongTxt txt={email.subject} chars={25} /></div>
+                <div className="mail-message"><LongTxt txt={email.body} chars={70} /></div>
+            </div>
             <div>{email.sentAt}</div>
         </div>
     </article>
