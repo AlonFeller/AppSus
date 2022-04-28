790c68ba3e955752defc1520a876e6c6d8cc5f48
Alon Feller <alon.feller16@gmail.com>
2022-04-28 19:13:11 +0300

    19:13

--- apps/email/cmps/email-preview.jsx
+++ apps/email/cmps/email-preview.jsx
@@ -6,7 +6,7 @@ export function EmailPreview({ email, onReadingEmail }) {
     return <article className="email-preview" >
         <div className="email-preview-msg" onClick={() => onReadingEmail(email.id)}>
             <div>To: {email.to}</div>
-            <div className="mail-subject"><LongTxt txt={email.subject} chars={25} /></div>
+            <div className="mail-subject"><LongTxt txt={email.subject} chars={15} /></div>
             <div className="mail-message"><LongTxt txt={email.body} chars={70} /></div>
             <div>{email.sentAt}</div>
         </div>
