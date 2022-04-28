81fc93634d26213d04e96971d64d4dba101d8586
Omer Levi <omerlevi223@gmail.com>
2022-04-28 20:22:08 +0300

    20:22

--- apps/email/cmps/email-preview.jsx
+++ apps/email/cmps/email-preview.jsx
@@ -5,13 +5,13 @@ export function EmailPreview({ email, onReadingEmail }) {
     console.log(email.message)
     return <article className="email-preview" >
         <div className="email-preview-msg" onClick={() => onReadingEmail(email.id)}>
-            <div class="btns">
+            <div class="btns flex justify-cnter align-center">
                 <button class="trash-btn">delete</button>
                 <button class="starred-btn">star</button>
                 <button class="read-btn">read/unread</button>
             </div>
             <div class="flex justify-cnter align-center">To: {email.to}</div>
-            <div class="message-container">
+            <div class="flex justify-cnter align-center message-container">
                 <div className="mail-subject"><LongTxt txt={email.subject} chars={25} /></div>
                 <div className="mail-message"><LongTxt txt={email.body} chars={70} /></div>
             </div>
