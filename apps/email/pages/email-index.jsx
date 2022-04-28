da4881e3ca2f0b4001c822c012d6de678a03fe9b
Omer Levi <omerlevi223@gmail.com>
2022-04-28 08:57:38 +0300

    08:57

--- apps/email/pages/email-index.jsx
+++ apps/email/pages/email-index.jsx
@@ -1,19 +1,14 @@
 import { emailService } from '../services/email.service.js'
-
 import { EmailList } from '../cmps/email-list.jsx'
 import { EmailFilter } from '../cmps/email-filter.jsx'
 import { EmailDetails } from '../pages/email-details.jsx'
-
-
-
-
-const { link } = ReactRouterDOM
-
+const { link } = ReactRouterDOM;
 
 export class EmailApp extends React.Component {
 
     state = {
         emails: [],
+        filteredEmails:[]
         // filterBy: null
     }
 
@@ -31,18 +26,34 @@ export class EmailApp extends React.Component {
 
     componentDidMount() {
         emailService.query()
-            .then(emails => this.setState({ emails }))
+            .then(emails => this.setState({ emails, filteredEmails:emails }))
     }
+
+    filterEmails = (filtersParams) => {
+        console.log(filtersParams)
+        const filteredEmails = this.state.emails.filter((email)=> {
+            console.log(email)
+            return (
+                email.body.toLowerCase().includes(filtersParams.search.toLowerCase())
+                // &&
+                // filtersParams.isRead
+
+            )
+        })
+
+        this.setState({filteredEmails})
+    } 
 
 
 
     render() {
-        const { emails } = this.state
+        const { filteredEmails } = this.state
         return <section className="email-app">
-            <form className="search-bar">
+            {/* <form className="search-bar">
                 <label htmlFor="search">search</label>
                 <input type="text" id="search" placeholder="Search" />
-            </form>
+            </form> */}
+            <EmailFilter filterEmails={this.filterEmails}/>
             <nav className="label-side-bar">
                 <button className="inbox-msgs">inbox</button>
                 <button className="sent-msgs">sent</button>
@@ -51,7 +62,7 @@ export class EmailApp extends React.Component {
             </nav>
             <section className="mails-container">
                 {/* <EmailList cars={this.carsToDisplay} onSelectCar={this.onSelectCar} /> */}
-                <EmailList emails={emails} />
+                <EmailList emails={filteredEmails} />
             </section>
         </section>
 
