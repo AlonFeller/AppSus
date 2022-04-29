098194892c13c312fa2d474cb9d2238679ed0034
Omer Levi <omerlevi223@gmail.com>
2022-04-29 17:05:51 +0300

    17:05

--- apps/email/cmps/email-preview.jsx
+++ apps/email/cmps/email-preview.jsx
@@ -1,21 +1,20 @@
 import { LongTxt } from '../cmps/email-long-text.jsx';
 
 
-export function EmailPreview({ email, onReadingEmail }) {
-    console.log(email.message)
+export function EmailPreview({ email }) {
     return <article className="email-preview" >
-        <div className="email-preview-msg" onClick={() => onReadingEmail(email.id)}>
-            <div class="btns flex">
-                <button class="trash-btn flex justify-cnter align-center">delete</button>
-                <button class="starred-btn flex justify-cnter align-center">star</button>
-                <button class="read-btn flex justify-cnter align-center">read/unread</button>
+        <div className="email-preview-msg" >
+            <div className="btns flex">
+                <button className="trash-btn flex justify-cnter align-center">delete</button>
+                <button className="starred-btn flex justify-cnter align-center">star</button>
+                <button className="read-btn flex justify-cnter align-center">read/unread</button>
             </div>
-            <div class="flex justify-cnter align-center">To: {email.to}</div>
-            <div class="message-container">
+            <div className="flex justify-cnter align-center">To: {email.to}</div>
+            <div className="message-container">
                 <div className="mail-subject flex justify-cnter align-center"><LongTxt txt={email.subject} chars={25} /></div>
                 <div className="mail-message flex justify-cnter align-center"><LongTxt txt={email.body} chars={70} /></div>
             </div>
-            <div class="flex justify-cnter align-center">{email.sentAt}</div>
+            <div className="flex justify-cnter align-center">{email.sentAt}</div>
         </div>
     </article>
 
