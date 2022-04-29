99754326124b93c01cc500432c848eba6946bb9f
Alon Feller <alon.feller16@gmail.com>
2022-04-29 17:10:53 +0300

    17:10

--- apps/keep/cmps/create-note.jsx
+++ apps/keep/cmps/create-note.jsx
@@ -19,14 +19,44 @@ export class CreateNote extends React.Component {
             isPinned: false
         }
     }
+    removeEventBus
 
+    componentDidMount() {
+        this.removeEventBus = eventBusService.on('toggleNoteCompose', (isOpen) => {
+            this.setState((prevState) => ({ ...prevState, isOpen }))
+        })
+    }
+
+    componentWillUnmount() {
+        this.removeEventBus();
+    }
+
+    handleFieldChange = ({ target }) => {
+        this.setState(prevState => ({ ...prevState, fields: { ...prevState.fields, [target.name]: target.value } }))
+    }
+
+    onChangeType = (ev, type) => {
+        ev.preventDefault()
+        this.setState(prevState => ({ ...prevState, type }))
+    }
+
+    onToggleExtraFields = (isComposeOpen) => {
+        eventBusService.emit('toggleScreen', isComposeOpen)
+        this.setState(prevState => ({ ...prevState, isOpen: isComposeOpen }))
+    }
 
 
     render() {
         <section className="note-card new">
-
-
-        </section>
+            <input type="text"
+                name="title"
+                placeholder="Title"
+                onChange={this.handleFieldChange}
+                onFocus={() => this.onToggleExtraFields(true)}
+                className={` ${(isOpen && type !== 'todos') ? 'compose-open' : ''}`}
+                value={this.state.fields.title} />
+        
+        </section >
 
     }
 
