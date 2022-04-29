be108cccf9bd536d860a30b18fc6b7eddc87d105
Omer Levi <omerlevi223@gmail.com>
2022-04-29 17:10:24 +0300

    17:10

--- apps/email/cmps/email-preview.jsx
+++ apps/email/cmps/email-preview.jsx
@@ -5,16 +5,16 @@ export function EmailPreview({ email }) {
     return <article className="email-preview" >
         <div className="email-preview-msg" >
             <div className="btns flex">
-                <button className="trash-btn flex justify-cnter align-center">delete</button>
-                <button className="starred-btn flex justify-cnter align-center">star</button>
-                <button className="read-btn flex justify-cnter align-center">read/unread</button>
+                <button className="trash-btn flex justify-center align-center">delete</button>
+                <button className="starred-btn flex justify-center align-center">star</button>
+                <button className="read-btn flex justify-center align-center">read/unread</button>
             </div>
-            <div className="flex justify-cnter align-center">To: {email.to}</div>
+            <div className="flex justify-center align-center">To: {email.to}</div>
             <div className="message-container">
-                <div className="mail-subject flex justify-cnter align-center"><LongTxt txt={email.subject} chars={25} /></div>
-                <div className="mail-message flex justify-cnter align-center"><LongTxt txt={email.body} chars={70} /></div>
+                <div className="mail-subject flex justify-center align-center"><LongTxt txt={email.subject} chars={25} /></div>
+                <div className="mail-message flex justify-center align-center"><LongTxt txt={email.body} chars={70} /></div>
             </div>
-            <div className="flex justify-cnter align-center">{email.sentAt}</div>
+            <div className="flex justify-center align-center">{email.sentAt}</div>
         </div>
     </article>
 
